b1ea4319c92b5c06dd97cc91aa794cbf1e69acfd
Michael Klamkin <michael@klamkin.com>
2026-05-12 03:23:00 -0400

    device data api

--- src/batch_api.jl
+++ src/batch_api.jl
@@ -56,6 +56,64 @@ function cuOptSolveBatchLP(
     )
 end
 
+function cuOptSolveBatchLPDeviceData(
+    problem,
+    settings,
+    batch_size,
+    objective_coefficients,
+    objective_coefficients_size,
+    constraint_lower_bounds,
+    constraint_lower_bounds_size,
+    constraint_upper_bounds,
+    constraint_upper_bounds_size,
+    variable_lower_bounds,
+    variable_lower_bounds_size,
+    variable_upper_bounds,
+    variable_upper_bounds_size,
+    objective_offsets,
+    objective_offsets_size,
+    solution_ptr,
+)
+    return ccall(
+        (:cuOptSolveBatchLPDeviceData, libcuopt),
+        cuopt_int_t,
+        (
+            cuOptOptimizationProblem,
+            cuOptSolverSettings,
+            cuopt_int_t,
+            Ptr{cuopt_float_t},
+            cuopt_int_t,
+            Ptr{cuopt_float_t},
+            cuopt_int_t,
+            Ptr{cuopt_float_t},
+            cuopt_int_t,
+            Ptr{cuopt_float_t},
+            cuopt_int_t,
+            Ptr{cuopt_float_t},
+            cuopt_int_t,
+            Ptr{cuopt_float_t},
+            cuopt_int_t,
+            Ptr{cuOptSolution},
+        ),
+        problem,
+        settings,
+        batch_size,
+        objective_coefficients,
+        objective_coefficients_size,
+        constraint_lower_bounds,
+        constraint_lower_bounds_size,
+        constraint_upper_bounds,
+        constraint_upper_bounds_size,
+        variable_lower_bounds,
+        variable_lower_bounds_size,
+        variable_upper_bounds,
+        variable_upper_bounds_size,
+        objective_offsets,
+        objective_offsets_size,
+        solution_ptr,
+    )
+end
+
 function cuOptGetBatchSize(solution, batch_size_ptr)
     return ccall(
         (:cuOptGetBatchSize, libcuopt),
